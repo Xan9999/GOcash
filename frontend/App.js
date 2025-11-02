85ee1bfa42bd590b8c5e1fc76704956e8fed7618
Nejc Mozetič <nejc.mozetic@predictleads.com>
2025-11-02 13:04:19 +0100

    Remove node_modules

--- frontend/App.js
+++ frontend/App.js
@@ -73,7 +73,7 @@ const btnImages = {
 
   // Auto-fetch projectId from app.json
   const projectId = Constants?.expoConfig?.extra?.eas?.projectId;
-  const API_BASE = 'http://192.168.0.115:5000'; // Keep this here as it's logic/config
+  const API_BASE = 'http://127.0.0.1:5000'; // Keep this here as it's logic/config
 
   // Notification setup (mobile only, with token registration after login)
   useEffect(() => {
